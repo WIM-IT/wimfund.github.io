8744ffed92fe2b7c830f3e3d8583120cb3ea57b8
wimfund <towujun@qq.com>
2019-01-22 08:59:38 +0800

    web

---
+++ zyro/7.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="zh">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title><?php echo htmlspecialchars(($seoTitle !== "") ? $seoTitle : "智德博客2"); ?></title>
+	<base href="{{base_url}}" />
+			<meta name="viewport" content="width=1200" />
+		<meta name="description" content="<?php echo htmlspecialchars(($seoDescription !== "") ? $seoDescription : ""); ?>" />
+	<meta name="keywords" content="<?php echo htmlspecialchars(($seoKeywords !== "") ? $seoKeywords : ""); ?>" />
+	<!-- Facebook Open Graph -->
+	<meta property="og:title" content="<?php echo htmlspecialchars(($seoTitle !== "") ? $seoTitle : "智德博客2"); ?>" />
+	<meta property="og:description" content="<?php echo htmlspecialchars(($seoDescription !== "") ? $seoDescription : ""); ?>" />
+	<meta property="og:image" content="<?php echo htmlspecialchars(($seoImage !== "") ? "{{base_url}}".$seoImage : ""); ?>" />
+	<meta property="og:type" content="article" />
+	<meta property="og:url" content="{{curr_url}}" />
+	<!-- Facebook Open Graph end -->
+		
+	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	<script src="js/jquery-1.11.3.min.js" type="text/javascript"></script>
+	<script src="js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="js/main.js?v=20190111020413" type="text/javascript"></script>
+
+	<link href="css/font-awesome/font-awesome.min.css?v=4.7.0" rel="stylesheet" type="text/css" />
+	<link href="css/site.css?v=20190111020414" rel="stylesheet" type="text/css" />
+	<link href="css/common.css?ts=1547715375" rel="stylesheet" type="text/css" />
+	<link href="css/7.css?ts=1547715375" rel="stylesheet" type="text/css" />
+	<ga-code/>
+	<script type="text/javascript">
+	window.useTrailingSlashes = true;
+</script>
+	
+	<link href="css/flag-icon-css/css/flag-icon.min.css" rel="stylesheet" type="text/css" />	
+	<!--[if lt IE 9]>
+	<script src="js/html5shiv.min.js"></script>
+	<![endif]-->
+
+	</head>
+
+
+<body><div class="root"><div class="vbox wb_container" id="wb_header">
+	
+<div class="wb_cont_inner"></div><div class="wb_cont_outer"></div><div class="wb_cont_bg"></div></div>
+<div class="vbox wb_container" id="wb_main">
+	
+<div class="wb_cont_inner"><div id="wb_element_instance84" class="wb_element wb-menu"><ul class="hmenu"><li><a href="" target="_self">主頁</a></li><li><a href="%E5%85%B3%E4%BA%8EWIM/" target="_self">关于WIM</a></li><li><a href="%E6%96%B0%E9%97%BB%E5%8A%A8%E6%80%81/" target="_self">新闻动态</a></li><li><a href="%E4%BA%BA%E6%89%8D%E6%8B%9B%E8%81%98/" target="_self">人才招聘</a></li><li><a href="%E8%81%94%E7%B3%BB%E6%88%91%E4%BB%AC/" target="_self">联系我们</a></li><li><a href="%E6%99%BA%E5%BE%B7%E5%8D%9A%E5%AE%A21/" target="_self">智德博客1</a></li><li class="active"><a href="%E6%99%BA%E5%BE%B7%E5%8D%9A%E5%AE%A22/" target="_self">智德博客2</a></li><li><a href="%E6%99%BA%E5%BE%B7%E5%8D%9A%E5%AE%A23/" target="_self">智德博客3</a></li><li><a href="%E6%99%BA%E5%BE%B7%E5%8D%9A%E5%AE%A24/" target="_self">智德博客4</a></li></ul><div class="clearfix"></div></div><div id="wb_element_instance85" class="wb_element wb-menu"><ul class="hmenu"><li><a href="" target="_self">主頁</a></li><li><a href="%E5%85%B3%E4%BA%8EWIM/" target="_self">证券投资</a></li><li><a href="%E8%82%A1%E6%9D%83%E6%8A%95%E8%B5%84/" target="_self">股权投资</a></li><li><a href="%E6%96%B0%E9%97%BB%E5%8A%A8%E6%80%81/" target="_self">新闻动态</a></li><li><a href="%E4%BA%BA%E6%89%8D%E6%8B%9B%E8%81%98/" target="_self">人才招聘</a></li><li><a href="%E8%81%94%E7%B3%BB%E6%88%91%E4%BB%AC/" target="_self">联系我们</a></li></ul><div class="clearfix"></div></div><div id="wb_element_instance86" class="wb_element wb_element_picture" title=""><img alt="gallery/qq图片20180117101109" src="gallery_gen/d578bd9b2cf7ccdaf613246a7d3fe944.png"></div><div id="wb_element_instance88" class="wb_element wb_text_element" style=" line-height: normal;"><h5 class="wb-stl-subtitle" style="text-align: center;"><span style="color:rgba(3,3,3,1);">智德博客  WIM Blog</span></h5>
+</div><div id="wb_element_instance89" class="wb_element wb_element_shape"><div class="wb_shp"></div></div><div id="wb_element_instance90" class="wb_element wb_text_element" style=" line-height: normal;"><h2 class="wb-stl-heading2">智德博客2018-09</h2><p>WIM Blog 2018-09</p></div><div id="wb_element_instance91" class="wb_element wb_text_element" style=" line-height: normal;"><p class="wb-stl-normal">       2018年诺贝尔生理学或医学奖颁给了大热的癌症免疫疗法，美国科学家James P.Allison 的CTLA-4和日本科学家Tasuku Honjo （本庶佑）的PD-1，两个突破性的疗法，众望所归。免疫疗法的概念最早在19世纪末~20世纪初出现，激活免疫系统可能可以攻击肿瘤细胞，期间科学家们有试图用细菌去感染病人，从而激发身体的免疫系统，然而效果有限。人类的免疫系统可以区分“自我”和“非自我”，“非自我”就代表着来自细菌、病毒等其他外来的攻击和威胁。T细胞是血液中白细胞的一种，它有特殊的受体，可以和入侵的病菌表面结构相互作用，从而引发免疫系统参与防御，但是还需要其他的蛋白质作为T细胞的加速剂，来引发全面的免疫反应。直到2010年，针对转移性黑色素瘤III期临床治疗的ipilimumab获得胜利，2011年，FDA正式获批ipilimumab单抗上市。现在是个PD-1时代，目前全球已经上市的PD-1/PD-L1共有6种，针对PD-1靶点的有3种除了BMS的Opdivo和MSD的Keytruda，近来FDA又获批了第三款PD-1，赛诺菲（Sanofi）和再生元（Regeneron）联合开发的Libtayo（cemiplimabrwlc）上市，用于治疗转移性皮肤鳞状细胞癌（CSCC）或者不能接受治愈性手术或放疗的局部晚期CSCC患者，这也是第一例针对晚期CSCC疗法。而BMS的Opdivo和MSD的Keytruda也已经相继于2018年6月15日和7月25日获批在国内上市。PD-1在国内目前形成了4+4的竞争格局，4家跨国药企：BMS、MSD、罗氏、阿斯利康，4家本国药企：恒瑞医药、百济神州、信达生物、君实生物。而四家均符合罕见病加速获批上市的条件，所以时间应该不会等太久，2018年末~2019年初获奖看到国产药企的PD-1上市。</p></div><div id="wb_element_instance92" class="wb_element" style="width: 100%;">
+			<?php
+				global $show_comments;
+				if (isset($show_comments) && $show_comments) {
+					renderComments(7);
+			?>
+			<script type="text/javascript">
+				$(function() {
+					var block = $("#wb_element_instance92");
+					var comments = block.children(".wb_comments").eq(0);
+					var contentBlock = $("#wb_main");
+					contentBlock.height(contentBlock.height() + comments.height());
+				});
+			</script>
+			<?php
+				} else {
+			?>
+			<script type="text/javascript">
+				$(function() {
+					$("#wb_element_instance92").hide();
+				});
+			</script>
+			<?php
+				}
+			?>
+			</div></div><div class="wb_cont_outer"></div><div class="wb_cont_bg"></div></div>
+<div class="vbox wb_container" id="wb_footer">
+	
+<div class="wb_cont_inner" style="height: 104px;"><div id="wb_element_instance87" class="wb_element wb_text_element" style=" line-height: normal;"><p>© Copyright 2014 智德投资管理有限公司 备案号 沪ICP备17049386号</p>
+</div><div id="wb_element_instance93" class="wb_element" style="text-align: center; width: 100%;"><div class="wb_footer"></div><script type="text/javascript">
+			$(function() {
+				var footer = $(".wb_footer");
+				var html = (footer.html() + "").replace(/^\s+|\s+$/g, "");
+				if (!html) {
+					footer.parent().remove();
+					footer = $("#wb_footer, #wb_footer .wb_cont_inner");
+					footer.css({height: ""});
+				}
+			});
+			</script></div></div><div class="wb_cont_outer"></div><div class="wb_cont_bg"></div></div><div class="wb_sbg"></div></div>{{hr_out}}</body>
+</html>
---
+++ zyro/8.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="zh">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title><?php echo htmlspecialchars(($seoTitle !== "") ? $seoTitle : "智德博客3"); ?></title>
+	<base href="{{base_url}}" />
+			<meta name="viewport" content="width=1200" />
+		<meta name="description" content="<?php echo htmlspecialchars(($seoDescription !== "") ? $seoDescription : ""); ?>" />
+	<meta name="keywords" content="<?php echo htmlspecialchars(($seoKeywords !== "") ? $seoKeywords : ""); ?>" />
+	<!-- Facebook Open Graph -->
+	<meta property="og:title" content="<?php echo htmlspecialchars(($seoTitle !== "") ? $seoTitle : "智德博客3"); ?>" />
+	<meta property="og:description" content="<?php echo htmlspecialchars(($seoDescription !== "") ? $seoDescription : ""); ?>" />
+	<meta property="og:image" content="<?php echo htmlspecialchars(($seoImage !== "") ? "{{base_url}}".$seoImage : ""); ?>" />
+	<meta property="og:type" content="article" />
+	<meta property="og:url" content="{{curr_url}}" />
+	<!-- Facebook Open Graph end -->
+		
+	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	<script src="js/jquery-1.11.3.min.js" type="text/javascript"></script>
+	<script src="js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="js/main.js?v=20190111020413" type="text/javascript"></script>
+
+	<link href="css/font-awesome/font-awesome.min.css?v=4.7.0" rel="stylesheet" type="text/css" />
+	<link href="css/site.css?v=20190111020414" rel="stylesheet" type="text/css" />
+	<link href="css/common.css?ts=1547715375" rel="stylesheet" type="text/css" />
+	<link href="css/8.css?ts=1547715375" rel="stylesheet" type="text/css" />
+	<ga-code/>
+	<script type="text/javascript">
+	window.useTrailingSlashes = true;
+</script>
+	
+	<link href="css/flag-icon-css/css/flag-icon.min.css" rel="stylesheet" type="text/css" />	
+	<!--[if lt IE 9]>
+	<script src="js/html5shiv.min.js"></script>
+	<![endif]-->
+
+	</head>
+
+
+<body><div class="root"><div class="vbox wb_container" id="wb_header">
+	
+<div class="wb_cont_inner"></div><div class="wb_cont_outer"></div><div class="wb_cont_bg"></div></div>
+<div class="vbox wb_container" id="wb_main">
+	
+<div class="wb_cont_inner"><div id="wb_element_instance94" class="wb_element wb-menu"><ul class="hmenu"><li><a href="" target="_self">主頁</a></li><li><a href="%E5%85%B3%E4%BA%8EWIM/" target="_self">关于WIM</a></li><li><a href="%E6%96%B0%E9%97%BB%E5%8A%A8%E6%80%81/" target="_self">新闻动态</a></li><li><a href="%E4%BA%BA%E6%89%8D%E6%8B%9B%E8%81%98/" target="_self">人才招聘</a></li><li><a href="%E8%81%94%E7%B3%BB%E6%88%91%E4%BB%AC/" target="_self">联系我们</a></li><li><a href="%E6%99%BA%E5%BE%B7%E5%8D%9A%E5%AE%A21/" target="_self">智德博客1</a></li><li><a href="%E6%99%BA%E5%BE%B7%E5%8D%9A%E5%AE%A22/" target="_self">智德博客2</a></li><li class="active"><a href="%E6%99%BA%E5%BE%B7%E5%8D%9A%E5%AE%A23/" target="_self">智德博客3</a></li><li><a href="%E6%99%BA%E5%BE%B7%E5%8D%9A%E5%AE%A24/" target="_self">智德博客4</a></li></ul><div class="clearfix"></div></div><div id="wb_element_instance95" class="wb_element wb-menu"><ul class="hmenu"><li><a href="" target="_self">主頁</a></li><li><a href="%E5%85%B3%E4%BA%8EWIM/" target="_self">证券投资</a></li><li><a href="%E8%82%A1%E6%9D%83%E6%8A%95%E8%B5%84/" target="_self">股权投资</a></li><li><a href="%E6%96%B0%E9%97%BB%E5%8A%A8%E6%80%81/" target="_self">新闻动态</a></li><li><a href="%E4%BA%BA%E6%89%8D%E6%8B%9B%E8%81%98/" target="_self">人才招聘</a></li><li><a href="%E8%81%94%E7%B3%BB%E6%88%91%E4%BB%AC/" target="_self">联系我们</a></li></ul><div class="clearfix"></div></div><div id="wb_element_instance96" class="wb_element wb_element_picture" title=""><img alt="gallery/qq图片20180117101109" src="gallery_gen/d578bd9b2cf7ccdaf613246a7d3fe944.png"></div><div id="wb_element_instance98" class="wb_element wb_text_element" style=" line-height: normal;"><h5 class="wb-stl-subtitle" style="text-align: center;"><span style="color:rgba(3,3,3,1);">智德博客  WIM Blog</span></h5>
+</div><div id="wb_element_instance99" class="wb_element wb_element_shape"><div class="wb_shp"></div></div><div id="wb_element_instance100" class="wb_element wb_text_element" style=" line-height: normal;"><h2 class="wb-stl-heading2">智德博客2018-12</h2>
+
+<p>WIM Blog 2018-12</p>
+</div><div id="wb_element_instance101" class="wb_element wb_text_element" style=" line-height: normal;"><p class="wb-stl-normal">    本月宏观面，国内宏观方面，1-11月全国规模以上工业企业实现利润同比增长11.8%，比1-10月下降1.8个百分点，11月当月同比下降1.8%，10月当月同比增长3.6%。11月规模以上工业企业利润当月同比增速出现了近3年来的首次负值，目前形势正在走向类似2014Q4至2015Q4工业品通缩的情景，当时PPI增速快速滑落至-5.9%，导致规上工业企业主营业务收入增速从8%以上快速降至1%，利润增速则降至负值。但目前主营业务利润率仍保持上行；成本率仍保持下行。显示降成本政策有所成效，但这难以抵消周期下行阶段收入下降的影响。对应到政策层面，如果问题出在收入端，而政策着力点在成本端，也会导致政策错配的风险。好在12月21日的2018年中央经济工作会议已经确定要“稳定总需求”、“逆周期调节”，政策错配风险正在下降。国企与民企在利润增长率上的差异继续趋于缩小，但在资产负债率上的差异11月出现异动：周期下行期间，资产相对负债降速更快导致资产负债率上升，这表明降杠杆不能只依靠限制债务，也要保持适度名义增长。分行业看，当前利润率的提升来源于去产能与环保限产、降成本（税费、融资难融资贵、控制劳动力成本等）等收入分配红利，有望在2019年持续。基于这一假设，可以预测2019年规模以上工业企业利润平均增长8.0%，趋势前低后高，2019Q1、Q2可能会继续负增长，直至通缩真正来临，宽松政策加力之后反弹回升。</p>
+</div><div id="wb_element_instance102" class="wb_element" style="width: 100%;">
+			<?php
+				global $show_comments;
+				if (isset($show_comments) && $show_comments) {
+					renderComments(8);
+			?>
+			<script type="text/javascript">
+				$(function() {
+					var block = $("#wb_element_instance102");
+					var comments = block.children(".wb_comments").eq(0);
+					var contentBlock = $("#wb_main");
+					contentBlock.height(contentBlock.height() + comments.height());
+				});
+			</script>
+			<?php
+				} else {
+			?>
+			<script type="text/javascript">
+				$(function() {
+					$("#wb_element_instance102").hide();
+				});
+			</script>
+			<?php
+				}
+			?>
+			</div></div><div class="wb_cont_outer"></div><div class="wb_cont_bg"></div></div>
+<div class="vbox wb_container" id="wb_footer">
+	
+<div class="wb_cont_inner" style="height: 104px;"><div id="wb_element_instance97" class="wb_element wb_text_element" style=" line-height: normal;"><p>© Copyright 2014 智德投资管理有限公司 备案号 沪ICP备17049386号</p>
+</div><div id="wb_element_instance103" class="wb_element" style="text-align: center; width: 100%;"><div class="wb_footer"></div><script type="text/javascript">
+			$(function() {
+				var footer = $(".wb_footer");
+				var html = (footer.html() + "").replace(/^\s+|\s+$/g, "");
+				if (!html) {
+					footer.parent().remove();
+					footer = $("#wb_footer, #wb_footer .wb_cont_inner");
+					footer.css({height: ""});
+				}
+			});
+			</script></div></div><div class="wb_cont_outer"></div><div class="wb_cont_bg"></div></div><div class="wb_sbg"></div></div>{{hr_out}}</body>
+</html>
